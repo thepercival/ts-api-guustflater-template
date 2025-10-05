46c2e71a892e1905b9c7a550a4b1890d6524c138
thepercival <cdkdev@pm.me>
2025-10-05 13:43:20 +0200

    feature(controllers): add

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+
 import Express from 'express';
 import morgan from 'morgan';
 
@@ -7,9 +8,8 @@ import {
   getAnimals,
   postAnimal,
   getAnimalByKind,
-  putAnimal,
-  
-} from './controllers/animalsController';
+  putAnimal  
+} from './controllers/animalsController.js';
 
 const app = Express();
 app.use(Express.json());
@@ -18,12 +18,9 @@ app.use(Express.json());
 // De logica voor het berekenen kan ook in deze source. 
 // Laad wel ts-sports-planning in via npm.
 
-// const animal: Animal = { kind: 'Cat', name: 'Mittens2' };
-
 // define api
 const api = new OpenAPIBackend({definition: "definitions/guustflater-openapi.yaml"});
-// console.log(api, animal);
-console.log({ kind: 'Cat', name: 'Mittens' });
+
 
 // register handlers
 api.register({
@@ -31,7 +28,6 @@ api.register({
   postAnimal,
   getAnimalByKind,
   putAnimal,
-  // validationFail and notFound are implemented inline here to keep framework-level behavior nearby
   validationFail: async (c, req: Express.Request, res: Express.Response) =>
     res.status(400).json({ err: c.validation.errors }),
   notFound: async (c, req: Express.Request, res: Express.Response) => res.status(404).json({ err: 'not found' }),
@@ -47,3 +43,5 @@ app.use((req, res) => api.handleRequest(req as Request, req, res));
 
 // start server
 app.listen(9000, () => console.info('api listening at http://localhost:9000'));
+
+
